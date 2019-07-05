f8bd164d35bfd75266fc78848ce9cd00f893b743
siena chen <clairechen6314@gmail.com>
2019-07-05 10:21:26 +0900

    delete

--- result_ID13/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv('new13_test.csv')
-X = df[['normal_heart','step']].values
-y = df['process_sleep'].values
-
-def data_predict(num):
-    result = (X,y) * num
-    return result
-print(len(data_predict(1)[0]))
